b10ca614abcd7c949eb1ef38e7202ff5d95c27f4
sms8-n8n <3anakebnt@gmail.com>
2025-09-24 12:47:36 +0100

    Sms8IoApi.credentials.ts

--- credentials/Sms8IoApi.credentials.ts
+++ credentials/Sms8IoApi.credentials.ts
@@ -1,24 +1,24 @@
 import {
-	ICredentialType,
-	INodeProperties,
+    ICredentialType,
+    INodeProperties,
 } from 'n8n-workflow';
 
 export class Sms8IoApi implements ICredentialType {
-	name = 'sms8IoApi';
-	displayName = 'SMS8.io API';
-	documentationUrl = 'https://sms8.io/sms8-api-documentation/';
-	properties: INodeProperties[] = [
-		{
-			displayName: 'API Key',
-			name: 'apiKey',
-			type: 'string',
-			typeOptions: {
-				password: true,
-			},
-			default: '',
-			placeholder: 'Enter your SMS8.io API key',
-			description: 'Get your API key from SMS8.io dashboard',
-			required: true,
-		},
-	];
+    name = 'sms8IoApi';
+    displayName = 'SMS8.io API';
+    documentationUrl = 'https://sms8.io/sms8-api-documentation/';
+    properties: INodeProperties[] = [
+        {
+            displayName: 'API Key',
+            name: 'apiKey',
+            type: 'string',
+            typeOptions: {
+                password: true,
+            },
+            default: '',
+            placeholder: 'Enter your SMS8.io API key',
+            description: 'Get your API key from SMS8.io dashboard',
+            required: true,
+        },
+    ];
 }
