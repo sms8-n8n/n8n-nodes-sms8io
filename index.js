3449697cc1243546e77b5660234676403c8006d7
sms8-n8n <3anakebnt@gmail.com>
2025-09-24 12:52:31 +0100

    index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 module.exports = {
-	nodes: [
-		'dist/nodes/Sms8Io/Sms8Io.node.js'
-	],
-	credentials: [
-		'dist/credentials/Sms8IoApi.credentials.js'
-	]
+    nodes: [
+        'dist/nodes/Sms8Io/Sms8Io.node.js'
+    ],
+    credentials: [
+        'dist/credentials/Sms8IoApi.credentials.js'
+    ]
 };
