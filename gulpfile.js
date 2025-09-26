71e78356ebcab0e221c26fda8a8a13005bfc1775
sms8-n8n <3anakebnt@gmail.com>
2025-09-26 11:11:31 +0100

    gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,22 @@
 const gulp = require('gulp');
 
+// Copy SVG icons from nodes
 gulp.task('build:icons', () => {
     return gulp.src('nodes/**/*.svg')
         .pipe(gulp.dest('dist/nodes'));
+});
+
+// Copy credential icons if any
+gulp.task('build:credentials', () => {
+    return gulp.src('credentials/**/*.svg')
+        .pipe(gulp.dest('dist/credentials'));
+});
+
+// Default build task
+gulp.task('build', gulp.parallel('build:icons', 'build:credentials'));
+
+// Handle errors
+gulp.on('error', (err) => {
+    console.error('Gulp error:', err);
+    process.exit(1);
 });
